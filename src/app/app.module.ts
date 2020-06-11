e252ecd8a7298262f4ac77ff800cd3987283a466
Raffy Anyelo <elraffy3@gmail.com>
2020-06-10 20:08:04 -0400

    getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// configuracion Firestore
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { DragableComponent } from './components/dragable/dragable.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent,
+    ProductsComponent,
+    DragableComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBucHwPfD1l_07v_ahb9sZUo8PklL8GU5M",
+    authDomain: "angularfirestore-64778.firebaseapp.com",
+    databaseURL: "https://angularfirestore-64778.firebaseio.com",
+    projectId: "angularfirestore-64778",
+    storageBucket: "angularfirestore-64778.appspot.com",
+    messagingSenderId: "457332416053",
+    appId: "1:457332416053:web:6a9e667f5ed388dd515b7c",
+    measurementId: "G-9T527JMP72"
+  }
 };
 
 /*
